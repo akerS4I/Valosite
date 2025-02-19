347121eb3de6b301bf17fe582810bdbe6b9de5c4
AKER <recrentic@gmail.com>
2025-02-19 15:13:22 +0500

    Home Page ready, Footer Added, Font changed

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import "../styles/components/Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>
+        Made for fun and to gain more experience in React, Typescript and SCSS
+        development
+      </h3>
+      <div className="contacts">
+        <p>Email: recrentic@gmail.com</p>
+        <p>Telegram: @akeraminai</p>
+        <p>Instagram: @akeraminai_sai</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,71 @@
 import "../styles/pages/Home.scss";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Navbar />
+      <div className="page-title">View Teams Attending VCT Masters Bangkok</div>
+      <div className="teams_container">
+        <div className="grid_container">
+          <div className="region_container">
+            <h1>Americas</h1>
+            <div className="logo_container">
+              <div className="team_wrapper">
+                <img src="/teams/g2.png" alt="G2" />
+                <h2>G2</h2>
+              </div>
+              <div className="team_wrapper">
+                <img src="/teams/sen.png" alt="SENTINELS" />
+                <h2>Sentinels</h2>
+              </div>
+            </div>
+          </div>
+          <div className="region_container">
+            <h1>China</h1>
+            <div className="logo_container">
+              <div className="team_wrapper">
+                <img src="/teams/edg.png" alt="EDG" />
+                <h2>EDG</h2>
+              </div>
+              <div className="team_wrapper">
+                <img src="/teams/te.png" alt="Trace Esports" />
+                <h2>Trace Esports</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid_container">
+          <div className="region_container">
+            <h1>EMEA</h1>
+            <div className="logo_container">
+              <div className="team_wrapper">
+                <img src="/teams/vit.png" alt="Vitality" />
+                <h2>Team Vitality</h2>
+              </div>
+              <div className="team_wrapper">
+                <img src="/teams/tl.png" alt="Team Liquid" />
+                <h2>Team Liquid</h2>
+              </div>
+            </div>
+          </div>
+          <div className="region_container">
+            <h1>Pacific</h1>
+            <div className="logo_container">
+              <div className="team_wrapper">
+                <img src="/teams/drx.png" alt="DRX" />
+                <h2>DRX</h2>
+              </div>
+              <div className="team_wrapper">
+                <img src="/teams/t1.png" alt="T1" />
+                <h2>T1</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
