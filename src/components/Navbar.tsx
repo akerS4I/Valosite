dd969844130eb2edee0feb18c7c42ded3c4b6dda
AKER <recrentic@gmail.com>
2025-02-20 15:17:36 +0500

    Fully Adaptive now aswell. Finished just as the first game started

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router";
+import { useState } from "react";
 import "../styles/components/Navbar.scss";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // Use boolean state
+
   return (
     <nav>
       <div className="container">
-        <img src="/Valosite/Valorant_logo_cropped.png" alt="" />
+        <img src="/Valosite/Valorant_logo_cropped.png" alt="Logo" />
         <Link className="title" to="/">
           Valosite
         </Link>
       </div>
-      <div className="links">
+
+      {/* Hamburger Menu */}
+      <div
+        className={`menu ${menuOpen ? "change" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Navigation Links */}
+      <div className={`links ${menuOpen ? "active" : ""}`}>
         <Link to="/news" className="nav_link">
           News
         </Link>
